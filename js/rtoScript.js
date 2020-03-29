f3232ad8e66643c704768f40d637c3663da0961f
gowxking <46034322+gowxking@users.noreply.github.com>
2020-03-29 23:17:24 +0300

    Add files via upload

---
+++ js/rtoScript.js
@@ -0,0 +1,68 @@
+calculate.onclick = function Calc() {
+    let angleGrad = +document.getElementById('angle').value;
+    let f = +document.getElementById('f').value;
+    let h = +document.getElementById('h').value;
+    let d = +document.getElementById('d').value;
+
+    let angleRad = angleGrad*Math.PI/180;
+
+    let l = 299792458/(f*1000000);
+    let k = l*(d+0.25*l);
+
+    let r1 = h/Math.sin(angleRad);
+    let r2 = d-r1;
+    let H = +Math.sin(angleRad)*r2;
+    let x0 = (1+k*(H-h))/(2*h*(k+Math.pow(h+H,2)));
+    let ax= (0.5*(d+0.5*l)*Math.pow((k*(k+4*h*H)), 0.5))/(Math.pow((k+Math.pow(h+H,2)),2));
+    let bx = (ax*Math.pow((k+Math.pow(h+H,2)) , 1/2))/(d+0.5*l);
+
+    console.log("l=" + l);
+    console.log("k=" + k);
+    console.log("angleRad=" + angleRad);
+    console.log("r1=" + r1);
+    console.log("r2=" + r2);
+    console.log("H=" + H);
+    console.log("x0=" + x0);
+    console.log("ax=" + ax);
+    console.log("bx=" + bx);
+
+    document.querySelector('.h0').setAttribute('value', Math.round(x0*100000)/100000 );
+    document.querySelector('.ax').setAttribute('value', Math.round(ax*100000)/100000 );
+    document.querySelector('.bx').setAttribute('value', Math.round(bx*100000)/100000 );
+}
+
+
+document.querySelector('.variant1').onclick = function () {
+    document.getElementById('angle').setAttribute('value', 2.5);
+    document.getElementById('f').setAttribute('value', 329);
+    document.getElementById('h').setAttribute('value', 4);
+    document.getElementById('d').setAttribute('value', 7000);
+}
+
+document.querySelector('.variant2').onclick = function () {
+    document.getElementById('angle').setAttribute('value', 2.7);
+    document.getElementById('f').setAttribute('value', 330);
+    document.getElementById('h').setAttribute('value', 8);
+    document.getElementById('d').setAttribute('value', 4000);
+}
+
+document.querySelector('.variant3').onclick = function () {
+    document.getElementById('angle').setAttribute('value', 3);
+    document.getElementById('f').setAttribute('value', 332);
+    document.getElementById('h').setAttribute('value', 12);
+    document.getElementById('d').setAttribute('value', 2000);
+}
+
+document.querySelector('.variant4').onclick = function () {
+    document.getElementById('angle').setAttribute('value', 3.2);
+    document.getElementById('f').setAttribute('value', 334);
+    document.getElementById('h').setAttribute('value', 15);
+    document.getElementById('d').setAttribute('value', 1000);
+}
+
+document.querySelector('.variant5').onclick = function () {
+    document.getElementById('angle').setAttribute('value', 3.5);
+    document.getElementById('f').setAttribute('value', 336);
+    document.getElementById('h').setAttribute('value', 17);
+    document.getElementById('d').setAttribute('value', 500);
+}
