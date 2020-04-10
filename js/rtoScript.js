6a1e512c4cdd2ad571e030519a21ad26fbcf14ac
gowxking <46034322+gowxking@users.noreply.github.com>
2020-04-10 23:33:09 +0300

    Update rtoScript.js

--- js/rtoScript.js
+++ js/rtoScript.js
@@ -8,12 +8,9 @@ calculate.onclick = function Calc() {
 
     let l = 299792458/(f*1000000);
     let k = l*(d+0.25*l);
-
-
     let r1 = h/Math.sin(angleRad);
     let r2 = d-r1;
     let H = +Math.sin(angleRad)*r2;
-
     let square = k + Math.pow(h+H, 2);
 
     let ax= (0.5*(d+0.5*l)*Math.pow((k*(k+4*h*H)), 0.5))/square;
@@ -21,20 +18,7 @@ calculate.onclick = function Calc() {
     let x0 = ( 0.66*ax + (k*(H-h))/(2*h*square) )/0.34;
 
 
-    console.log("l=" + l);
-    console.log("k=" + k);
-    console.log("angleRad=" + angleRad);
-    console.log("r1=" + r1);
-    console.log("r2=" + r2);
-    console.log("H=" + H);
-    console.log("x0=" + x0);
-    console.log("ax=" + ax);
-    console.log("bx=" + bx);
-
-    console.log("sin=" + Math.sin(angleRad));
-    console.log("square=" + square);
-
-    if (isNaN(ax)) {
+    if (isNaN(ax, bx ,x0)) {
         alert('Введи данные -_-')
     } 
     else if (isNaN(bx)) {
